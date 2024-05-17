d88affccdc289ed3de2c31d8af2b5b897285b190
Jose Alejandro Alvarez Mendoza <josealejandro.alvarez848@comunidadunir.net>
2024-05-16 22:06:10 -0500

    Actividad_1: Separar estilos por componentes

--- src/views/Category.jsx
+++ src/views/Category.jsx
@@ -18,7 +18,7 @@ export const Category = ({category}) => {
         <div className="col-sm-6 col-md-4 col-lg-3">
             <Card className="m-2" style={{ width: '18rem', cursor: 'pointer' }} onClick={handleCardClick}>
                 <div style={{ height: '150px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <Card.Img variant="top" src={images[category]} className="product-image" />
+                    <Card.Img variant="top" src={images[category]} className="product__image" />
                 </div>
                 <Card.Body className="d-flex flex-column justify-content-between">
                     <Card.Title className="text-center text-capitalize">{category}</Card.Title>
